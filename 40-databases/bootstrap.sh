c77a6067acd63e883a0d284a63e28fe921d6960b
mamatha-ravi <mamata.r@gmail.com>
2026-03-05 19:14:44 +0530

    infra

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-component = $1
+
+component=$1
 dnf install ansible -y
 
 cd /home/ec2-user
 git clone https://github.com/mamatha-ravi/ansible-roboshop-roles-tf.git
 
-cd ansible-roboshop-roles-tf/
+cd ansible-roboshop-roles-tf
 ansible-playbook -e component=$component roboshop.yaml
