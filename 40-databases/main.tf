18a0a3d97e1229fe271adfc3bd29b3a80a826511
mamatha-ravi <mamata.r@gmail.com>
2026-03-06 19:05:44 +0530

    infra

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -115,7 +115,7 @@ resource "aws_instance" "rabbitmq" {
   instance_type = "t3.micro"
   subnet_id = local.database_subnet_id
   vpc_security_group_ids = [local.rabbitmq_sg_id]
-  #iam_instance_profile = aws_iam_instance_profile.rabbitmq.name
+
   tags = merge(
     {
         Name = "${var.project}-${var.environment}-rabbitmq"
@@ -126,23 +126,25 @@ resource "aws_instance" "rabbitmq" {
 
 resource "terraform_data" "rabbitmq" {
   triggers_replace = [
-    aws_instance.rabbitmq.id,
-      ]
- connection {
+    aws_instance.rabbitmq.id
+  ]
+
+  connection {
     type     = "ssh"
     user     = "ec2-user"
     password = "DevOps321"
     host     = aws_instance.rabbitmq.private_ip
-   
   }
-   provisioner "file" {
-    source      = "bootstrap.sh"  # Local file path
-    destination = "/tmp/bootstrap.sh"   # Remote file path
-   }
+
+  provisioner "file" {
+    source      = "bootstrap.sh" # Local file path
+    destination = "/tmp/bootstrap.sh"    # Destination path on the remote machine
+  }
+
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}"
+        "sudo sh /tmp/bootstrap.sh rabbitmq dev"
     ]
   }
 }
