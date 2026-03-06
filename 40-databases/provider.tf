9867e9083501c5eda33372b3aa10972f98277386
mamatha-ravi <mamata.r@gmail.com>
2026-03-06 18:27:43 +0530

    infra

--- 40-databases/provider.tf
+++ 40-databases/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
     backend "s3" {
     bucket         = "remote-state-s3-88s-dev"
-    key            = "roboshop-dev-db"
+    key            = "roboshop-${var.environment}-db"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile   = true
