eb078002ec3f68f37c212f7823a29c93959689bd
mamatha-ravi <mamata.r@gmail.com>
2026-03-09 15:23:08 +0530

    infra

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -1,35 +1,45 @@
-resource "aws_route53_record" "mongodb" {
+resource "aws_route53_record" "databases" {
+  for_each = aws_instance.database
   zone_id = var.zone_id
-  name    = "mongodb-${var.environment}.${var.domain_name}"
+  name    = "${each.key}-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = "1"
-  records = [aws_instance.mongodb.private_ip]
+  records = [each.value.private_ip]
   allow_overwrite = true
 }
 
-resource "aws_route53_record" "redis" {
-  zone_id = var.zone_id
-  name    = "redis-${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = "1"
-  records = [aws_instance.redis.private_ip]
-  allow_overwrite = true
-}
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = var.zone_id
+#   name    = "mongodb-${var.environment}.${var.domain_name}"
+#   type    = "A"
+#   ttl     = "1"
+#   records = [aws_instance.mongodb.private_ip]
+#   allow_overwrite = true
+# }
 
-resource "aws_route53_record" "mysql" {
-  zone_id = var.zone_id
-  name    = "mysql-${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = "1"
-  records = [aws_instance.mysql.private_ip]
-  allow_overwrite = true
-}
+# resource "aws_route53_record" "redis" {
+#   zone_id = var.zone_id
+#   name    = "redis-${var.environment}.${var.domain_name}"
+#   type    = "A"
+#   ttl     = "1"
+#   records = [aws_instance.redis.private_ip]
+#   allow_overwrite = true
+# }
 
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = var.zone_id
-  name    = "rabbitmq-${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = "1"
-  records = [aws_instance.rabbitmq.private_ip]
-  allow_overwrite = true
-}
+# resource "aws_route53_record" "mysql" {
+#   zone_id = var.zone_id
+#   name    = "mysql-${var.environment}.${var.domain_name}"
+#   type    = "A"
+#   ttl     = "1"
+#   records = [aws_instance.mysql.private_ip]
+#   allow_overwrite = true
+# }
+
+# resource "aws_route53_record" "rabbitmq" {
+#   zone_id = var.zone_id
+#   name    = "rabbitmq-${var.environment}.${var.domain_name}"
+#   type    = "A"
+#   ttl     = "1"
+#   records = [aws_instance.rabbitmq.private_ip]
+#   allow_overwrite = true
+# }
