3ba74a8f97fef962e240384b9b66657355ae30db
mamatha-ravi <mamata.r@gmail.com>
2026-03-10 15:19:36 +0530

    infra

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -24,9 +24,7 @@ data "aws_ami" "joindevops" {
     values = ["hvm"]
   }
 }
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project}/${var.environment}/private_subnet_ids"
-}
+
 data "aws_ssm_parameter" "catalogue_sg_id" {
     name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
