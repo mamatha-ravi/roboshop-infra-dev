1ec04f823ced15150f713f1317f17f452abfb39c
mamatha-ravi <mamata.r@gmail.com>
2026-03-10 15:11:10 +0530

    infra

---
+++ 60-catalogue/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+  private_subnet_ids = [data.aws_ssm_parameter.private_subnet_ids.value]
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+  ami_id = data.aws_ami.joindevops
+  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
+}
