f4f81dfe06211ea9dce5a553e34215bb1e27c816
mamatha-ravi <mamata.r@gmail.com>
2026-03-10 15:45:27 +0530

    infra

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -58,7 +58,7 @@ resource "aws_lb_target_group" "catalogue" {
   name     = "${var.project}-${var.environment}-catalogue"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = local.vpc.id
+  vpc_id   = local.vpc_id
  deregistration_delay = 60
 
   health_check {
